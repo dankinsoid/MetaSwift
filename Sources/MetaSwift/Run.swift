349d1df66bc6775f37d964a0bd6373a97cceb19b
dankinsoid <voidilov@gmail.com>
2022-02-18 14:37:33 +0300

    run

--- Sources/MetaSwift/Run.swift
+++ Sources/MetaSwift/Run.swift
@@ -51,28 +51,71 @@ public struct CodeGenerator {
 	public func generate() throws {
 		let collector = DeclarationCollector()
 		if let sources = sources {
-
+			for url in try allSwiftFiles(at: sources) {
+				do {
+					let syntax = try SyntaxParser.parse(url)
+					collector.walk(syntax)
+				} catch {
+					print("Parsing error for \(url): \(error)")
+				}
+			}
 		}
 		for generator in generators {
-
+			let code = generator.generator(collector)
+			var text = ""
+			code.write(to: &text)
+			if FileManager.default.fileExists(atPath: generator.url.path) {
+				try FileManager.default.removeItem(at: generator.url)
+			} else {
+				try FileManager.default.createDirectory(at: generator.url.deletingLastPathComponent(), withIntermediateDirectories: true)
+			}
+			guard FileManager.default.createFile(atPath: generator.url.path, contents: Data(text.utf8)) else {
+				throw Failure.cannotWriteFile
+			}
 		}
 	}
 
 	public func monitor() async throws {
 		guard let monitor = monitor else {
-			return
+			throw Failure.noURLForMonitoring
 		}
-		let fileMonitor = try FileMonitor(url: monitor)
-
-		while true {
-			if Task.isCancelled {
-				return
+		var monitors: [FileMonitor] = []
+		for url in try allSwiftFiles(at: monitor) {
+			let fileMonitor = try FileMonitor(url: url)
+			monitors.append(fileMonitor)
+			fileMonitor.monitor { _ in
+				try? self.generate()
 			}
 		}
+		while true {
+			guard !Task.isCancelled else {
+				break
+			}
+		}
+	}
+
+	private func allSwiftFiles(at url: URL) throws -> [URL] {
+		guard let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) else {
+			throw Failure.cannotCreateEnumerator
+		}
+		var files = [URL]()
+		for case let fileURL as URL in enumerator {
+			let fileAttributes = try fileURL.resourceValues(forKeys: [.isRegularFileKey])
+			if fileAttributes.isRegularFile == true, fileURL.pathExtension == "swift" {
+				files.append(fileURL)
+			}
+		}
+		return files
 	}
 
 	public struct FileGenerator {
 		public var url: URL
 		public var generator: (DeclarationCollector) -> Syntax
+	}
+
+	public enum Failure: String, LocalizedError {
+		case cannotCreateEnumerator
+		case noURLForMonitoring
+		case cannotWriteFile
 	}
 }
