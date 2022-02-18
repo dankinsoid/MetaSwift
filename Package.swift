19cca4ff55d298d7ea6b97996caa9d18bfde1f88
dankinsoid <voidilov@gmail.com>
2022-02-18 14:54:04 +0300

    0.3.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         .library(name: "MetaSwift", targets: ["MetaSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
         .package(name: "SwiftSemantics", url: "https://github.com/dankinsoid/SwiftSemantics.git", .exact("5.5.1"))
     ],
@@ -19,8 +18,7 @@ let package = Package(
             name: "MetaSwift",
             dependencies: [
                 "SwiftSyntax",
-                "SwiftSemantics",
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                "SwiftSemantics"
             ]
         ),
         .testTarget(name: "MetaSwiftTests", dependencies: ["MetaSwift"]),
